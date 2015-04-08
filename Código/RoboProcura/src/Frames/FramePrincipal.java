4e09b2ebb80cd052cd4510f531b50ed054d7c58c
Razart007 <Rafael.RedSky@gmail.com>
2015-04-08 19:43:47 -0300

    Algumas alterações do FramePrincipal e criação da pasta de imagens

--- Código/RoboProcura/src/Frames/FramePrincipal.java
+++ Código/RoboProcura/src/Frames/FramePrincipal.java
@@ -6,20 +6,23 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
 
 public class FramePrincipal extends JFrame{
 	JPanel ambiente;
 	JMenuBar barraMenus;
 	JMenu arquivo, executar;
 	JMenuItem sair, novo, abrir, salvar;
-	JButton executarBu;
-	
+	JButton jBuExecutar;
+	JLabel lbRobo;
 
 	public FramePrincipal(){
 		inicializaFrame();
@@ -27,6 +30,12 @@ public class FramePrincipal extends JFrame{
 	}
 	
 	private void inicializaFrame(){
+		try {  
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");  
+		} catch (Exception e) {  
+			e.printStackTrace();  
+		}  
+
 		this.setTitle("Robo à Procura");
 		this.setIconImage(Toolkit.getDefaultToolkit().getImage("Imagens/Robo.png"));
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -45,7 +54,7 @@ public class FramePrincipal extends JFrame{
 		novo = new JMenuItem("Novo",KeyEvent.VK_N);
 		abrir = new JMenuItem("Abrir",KeyEvent.VK_O);
 		salvar = new JMenuItem("Salvar",KeyEvent.VK_S);
-		sair = new JMenuItem("Salvar",KeyEvent.VK_E);
+		sair = new JMenuItem("Sair",KeyEvent.VK_E);
 		
 		arquivo = new JMenu("Arquivo");
 		arquivo.add(novo);
@@ -93,12 +102,19 @@ public class FramePrincipal extends JFrame{
 		ambiente.setSize(100, 200);
 		ambiente.setBackground(Color.WHITE);
 		add(ambiente);
-		ambiente.setBounds(50, 50, 150, 250);
+		ambiente.setBounds(10, 10, 995, 540);
 		
-		executarBu = new JButton("Executar");
-		executarBu.setSize(100,100);
-		add(executarBu);
-		executarBu.setBounds(300, 300, 100, 100);
+		jBuExecutar = new JButton("Executar");
+		jBuExecutar.setSize(100, 30);
+		add(jBuExecutar);
+		jBuExecutar.setBounds(480, 560, 100, 30);
+		
+	    //ImageIcon imRobo = new ImageIcon(getClass().getResource("/Imagens/Robo.png"));  
+		
+		lbRobo = new JLabel("Robo");
+		lbRobo.setSize(100, 100);
+		ambiente.add(lbRobo);
+		lbRobo.setBounds(10, 200, 100, 100);
 	}
 	
 	public static void main(String args[]){
